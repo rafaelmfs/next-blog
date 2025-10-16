1495edea12714347492cc6c412f295b6835f568c
Rafael Marques <rafael.marques-98@hotmail.com>
2025-10-16 01:02:57 -0300

    reorganizando pastas

--- src/app/(home)/components/categories-button.tsx
+++
@@ -1,8 +0,0 @@
-import clsx from "clsx"
-import type { ComponentProps } from "react"
-
-type CategoriesButtonProps = ComponentProps<"button">
-
-export function CategoriesButton({ className, ...props }: CategoriesButtonProps) {
-  return <button {...props} className={clsx(["rounded-sm bg-sky-400 font-bold py-1 px-1 lg:px-3 lg:py-2 text-xs lg:text-base flex items-center justify-center text-white border border-sky-400 hover:bg-sky-600 dark:hover:bg-sky-300 cursor-pointer transition-all", className])} />
-};
--- src/app/blog/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-
-export default async function PostDetails() {
-  return (
-    <div>
-    </div>
-  );
-}
